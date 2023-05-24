bce533fb4013104301649ea5dfb8e6a7f92fc1c2
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-05-24 16:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function onesDigit(n) {
   // your code here
+	let digit = n % 10;
+	// while(n > 0 )
+	// 	{
+	// 		let digit =  ;
+	// 		n = n / 10;
+	// 	}
+	return digit;
 }
 
 // Do not change the code below
